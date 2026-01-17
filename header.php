465e1b4a6d3c9c86253e7c0fba0f5e4843609d53
Muhammad Younus <muhammadddyounus@gmail.com>
2026-01-17 18:21:30 +0500

    Add custom favicon and ensure Full Flavor image visibility

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png" type="image/png">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
